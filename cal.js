abd70d8e1566c18d095ff4a3498094956f4ab1aa
AlgebraEmma <83509406+AlgebraEmma@users.noreply.github.com>
2024-02-24 02:45:41 +0700

    Update cal.js

--- cal.js
+++ cal.js
@@ -3,36 +3,29 @@ function cal(){
 let age = document.getElementById('Age');
 let height = document.getElementById('height');
 let Scr = document.getElementById('Scr');
-let output = document.getElementById('output');
+let outputCrcl = document.getElementById('outputCrcl');
+let outputBmi = document.getElementById('outputBmi');
+let outDetect = document.getElementById('outDetect');
 let weigh = document.getElementById('weigh');
 var sex = document.getElementsByName('sex');
     age = Number(age.value);
     height = Number(height.value);
     Scr = Number(Scr.value);
     weigh = Number(weigh.value);
-    let crcl = ((140-age)*weigh)/(Scr*72) ;
-    let outHTML = '';
-    
-    // for(let i=0;i<=sex.length;i++){
-    //     if(sex[i].checked){
-    //         output.innerHTML += "<p>" + sex[i].value + "</p><br>"
-    //     }
-    // }
+	let crcl=((140-age)*weigh)/(Scr*72) ;
+	let bmi =(weigh)/((height/100)*(height/100)) ;
         if(sex[0].checked){
-            outHTML += "<p>" ;
-            outHTML += crcl*0.85 ;
-            outHTML += "ml/min    (female x 0.85)</p>";
+       		crcl=crcl*0.85;
         }
         else if (sex[1].checked){ 
-            outHTML += "<p>" ;
-            outHTML += crcl ;
-            outHTML += "ml/min      </p>";
+            crcl=crcl;
         }
         else {
-            outHTML = "plasee chose sex"
+            crcl = "---plasee chose sex---";
         }    
-    outHTML += "<br>" + "age ="+ age +" , " + "weigh ="+ weigh +" , " + "Scr ="+ Scr ;
-    output.innerHTML = outHTML; 
+    outDetect.innerHTML =  "age ="+ age +" , " + "weigh ="+ weigh +" , " + "Scr ="+ Scr +" , "+"height ="+height;
+    outputCrcl.innerHTML = "Crcl ="+crcl+"ml/min"; 
+	outputBmi.innerHTML = "BMI = "+bmi+"kg/m<sup>2</sup>";
 }
 
 button.addEventListener("click",cal)
